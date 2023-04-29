18da5d3dc215cd8f844fdc98c645a7daef7d79dd
agarcia4459 <agarcia4459@hotmail.com>
2023-04-29 17:56:11 +0200

    cumplir test calculateApiBadRequest

--- src/main/java/com/example/calculadora/api/CalculadoraController.java
+++ src/main/java/com/example/calculadora/api/CalculadoraController.java
@@ -1,9 +1,13 @@
 package com.example.calculadora.api;
 
+import java.util.regex.Pattern;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +20,7 @@ import io.corp.calculator.TracerImpl;
 @RestController
 public class CalculadoraController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CalculadoraController.class);
+	private static final Pattern operacionesValidasPattern = Pattern.compile("[0-9]+(\\+|-)[0-9]+");
 
 	private final CalculadoraService calcService;
 
@@ -28,9 +33,21 @@ public class CalculadoraController {
 	@ResponseBody
 	public ResponseEntity<Double> calcular(@PathVariable("operacion") final String operacion) {
 		LOGGER.debug("Inicio operacion {}", operacion);
+		if (!isValid(operacion)) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 		final TracerImpl tracerAPI = new TracerImpl();
 		final double resultado = calcService.calcular(operacion);
 		tracerAPI.trace(resultado);
 		return ResponseEntity.ok(resultado);
+	}
+
+	private boolean isValid(final String operacion) {
+		if (ObjectUtils.isEmpty(operacion) || !operacionesValidasPattern.matcher(operacion).find()) {
+			return false;
+		} else {
+
+			return true;
+		}
 	}
 }
